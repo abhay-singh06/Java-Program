e1f51ab004995a5b8b3ba3fed7c886bbb69d9024
Abhay Singh <abhay75650@gmail.com>
2026-02-09 00:10:03 +0530

    First Commit

---
+++ Basic/program1.java
@@ -0,0 +1,15 @@
+class program1{
+public static void main(String[] args)
+{
+System.out.println("first program");
+int x=12; 
+System.out.println(x);
+Box b=new Box();
+System.out.println(b);
+System.out.println(b.p);
+System.out.println(b.q);
+System.out.println(b.r);
+System.out.println(b.s);
+System.out.println("====program ends====");
+}
+}
